ea1d09cc337a55bb2bb5394adf90adf502616dbe
Thomas Sample <thomas@mfrwdesign.com>
2018-12-21 13:50:17 -0500

    Use https in User Agent header

--- lib/spreadshirt_client.rb
+++ lib/spreadshirt_client.rb
@@ -59,7 +59,7 @@ module SpreadshirtClient
       # Will this work properly for passing in the user agent? Looks like yes based on:
       # https://github.com/rest-client/rest-client/blob/f450a0f086f1cd1049abbef2a2c66166a1a9ba71/spec/unit/request_spec.rb#L623
       # TODO: move this back to the AthleteOriginals app when things are working again
-      opts[:user_agent] ||= "Athlete-Originals/2.0 (http://www.athleteoriginals.com; support@athleteoriginals.com)"
+      opts[:user_agent] ||= "Athlete-Originals/2.0 (https://www.athleteoriginals.com; support@athleteoriginals.com)"
 
       opts[:content_type] ||= "application/xml"
 
